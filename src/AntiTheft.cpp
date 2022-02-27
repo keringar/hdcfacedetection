b192417fa1710ee171150a560b29c532b37b2a00
bjericks <bjericks@ucsd.edu>
2022-02-26 17:38:32 -0800

    Added Pi Camera, WiringPi integration

--- src/AntiTheft.cpp
+++ src/AntiTheft.cpp
@@ -1,8 +1,12 @@
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/image_processing.h>
 #include <dlib/image_io.h>
+#include <dlib/opencv.h>
+#include <raspicam/raspicam_cv.h>
+#include <wiringPi.h>
 
 #include <iostream>
+#include <ctime>
 
 #include "ap.h"
 #include "bm.h"
@@ -13,6 +17,10 @@ using namespace std;
 
 #define ITEM_MEM_SIZE 136
 #define LEVEL_SIZE 201
+
+#define PIN_INPUT 0
+#define PIN_DIP_RED 8
+#define PIN_DIP_GRN 9
 
 // 68*2 = 136 random vectors. One random vector per feature
 // Features are the x and y coordinates of extracted feature from dlib
@@ -28,6 +36,9 @@ bvector<> rider_vector, not_rider_vector;
 frontal_face_detector detector;
 shape_predictor sp;
 
+raspicam::RaspiCam_Cv Camera;
+cv::Mat image;
+
 // Interrupt handler runs concurrently with rest of program
 // Gets image from camera
 // Extracts features
@@ -36,26 +47,38 @@ shape_predictor sp;
 // Output detected class. Green for rider. Red for not rider.
 extern "C" void interrupt_handler(void) {
   // Get Image From Camera
-  array2d<rgb_pixel> img;
-  load_image(img, "images_not_rider/Aaron_Eckhart/Aaron_Eckhart_0001.jpg");
-  pyramid_up(img);
+//   load_image(img, "images_not_rider/Aaron_Eckhart/Aaron_Eckhart_0001.jpg");
+//   pyramid_up(img);
 
+  //capture
+  cout << "Capturing image" << endl;
+  Camera.grab();
+  //extract the image in rgb format
+  Camera.retrieve ( image );//get camera image
+
+  cv_image<rgb_pixel> img(image);
+
+  cout << "Extracting facial features from image" << endl;
   std::vector<rectangle> dets = detector(img);
 
   // If detected no faces, set all coordinates to 0
   full_object_detection transformed_shape;
-  if (dets.size() != 0) {
-      // Just extract first face landmarks
-      full_object_detection shape = sp(img, dets[0]);
-
-      // We can also extract copies of each face that are cropped, rotated upright,
-      // and scaled to a standard size as shown here:
-      matrix<rgb_pixel> face_chip;
-      auto chip_details = get_face_chip_details(shape);
-      extract_image_chip(img, chip_details, face_chip);
-
-      transformed_shape = map_det_to_chip(shape, chip_details);
+  if (dets.size() == 0) {
+    digitalWrite(PIN_DIP_GRN, 0);
+    digitalWrite(PIN_DIP_RED, 0);
+    return;
   }
+  cout << "Face detected in image" << endl;
+  // Just extract first face landmarks
+  full_object_detection shape = sp(img, dets[0]);
+
+  // We can also extract copies of each face that are cropped, rotated upright,
+  // and scaled to a standard size as shown here:
+  matrix<rgb_pixel> face_chip;
+  auto chip_details = get_face_chip_details(shape);
+  extract_image_chip(img, chip_details, face_chip);
+
+  transformed_shape = map_det_to_chip(shape, chip_details);
 
   // Copy extracted feature coordinates to HDC vector
   std::vector<uint8_t> encoded_vector_sum(10000);
@@ -63,13 +86,8 @@ extern "C" void interrupt_handler(void) {
 
   for (size_t i = 0; i < 68; i++) {
       size_t x, y;
-      if (dets.size() != 0) {
-          x = min(max((int)transformed_shape.part(i).x(), 0), 200);
-          y = min(max((int)transformed_shape.part(i).y(), 0), 200);
-      } else {
-          x = 0;
-          y = 0;
-      }
+      x = min(max((int)transformed_shape.part(i).x(), 0), 200);
+      y = min(max((int)transformed_shape.part(i).y(), 0), 200);
 
       encoded_x.clear();
       encoded_x.bit_xor(level_vectors[x], item_memory_vectors[i*2]);
@@ -102,9 +120,13 @@ extern "C" void interrupt_handler(void) {
   if (rider_hamming > not_rider_hamming) {
     // Rider Detected
     cout << "Rider Detected" << endl;
+    digitalWrite(PIN_DIP_GRN, 1);
+    digitalWrite(PIN_DIP_RED, 0);
   } else {
     // Not Rider Detected
     cout << "Rider Not Detected" << endl;
+    digitalWrite(PIN_DIP_RED, 1);
+    digitalWrite(PIN_DIP_GRN, 0);
   }
 } 
 
@@ -113,6 +135,21 @@ int main(int argc, char* argv[]) {
     cerr << "Usage: ./AntiTheft ./shape_predictor_68_face_landmarks.dat ./random_seed_vectors.txt ./trained_model.txt" << endl;
     return 1;
   }
+
+  wiringPiSetup();
+  pinMode(PIN_INPUT, INPUT);
+  pinMode(PIN_DIP_GRN, OUTPUT);
+  pinMode(PIN_DIP_RED, OUTPUT);
+
+  //set camera params
+  // Camera is 3280 x 2464 pixels
+  Camera.set( CV_CAP_PROP_FRAME_WIDTH, 1920);
+  Camera.set( CV_CAP_PROP_FRAME_HEIGHT, 1080);
+  Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
+  //Open camera
+  cout<<"Opening Camera..."<<endl;
+  if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+    
 
   // Load face position dector
   detector = get_frontal_face_detector();
@@ -165,7 +202,12 @@ int main(int argc, char* argv[]) {
 
   cout << "Done loading trained model vectors" << endl;
 
-  // Setup interrupt handling
+  // Enable interrupt handling
   // ...
-  interrupt_handler();
+  wiringPiISR(PIN_INPUT, INT_EDGE_RISING, interrupt_handler);
+
+  // Infinite loop to catch interrupts
+  while (1) {
+      // Wait for interrupt (in idle state?)
+  }
 }
